6b26b0089c80c8e5de73ebb362641e575905c9fa
Jin Park <forjin@vpac-innovations.com.au>
2016-06-01 09:50:26 +1000

    variablerized common-data database

--- import_default_data.sh
+++ import_default_data.sh
@@ -10,23 +10,24 @@ done
 
 echo "UPDATE visualizations SET privacy = 'public';" | psql -U postgres -h postgres -d carto_db_development
 # GRANT imported table to SELECT access
+COMMON_DATA_DATABASE=cartodb_dev_user_3be5d2dc-eab4-4067-b6a9-f32b3b26bc8b_db
 
-echo "GRANT SELECT ON public.sa1_2011_aust TO publicuser;" | psql -U postgres -h postgres -d cartodb_dev_user_3be5d2dc-eab4-4067-b6a9-f32b3b26bc8b_db
-echo "GRANT SELECT ON public.sa2_2011_aust TO publicuser;" | psql -U postgres -h postgres -d cartodb_dev_user_3be5d2dc-eab4-4067-b6a9-f32b3b26bc8b_db
-echo "GRANT SELECT ON public.sa3_2011_aust TO publicuser;" | psql -U postgres -h postgres -d cartodb_dev_user_3be5d2dc-eab4-4067-b6a9-f32b3b26bc8b_db
-echo "GRANT SELECT ON public.sa4_2011_aust TO publicuser;" | psql -U postgres -h postgres -d cartodb_dev_user_3be5d2dc-eab4-4067-b6a9-f32b3b26bc8b_db
-echo "GRANT SELECT ON public.lga11aaust TO publicuser;" | psql -U postgres -h postgres -d cartodb_dev_user_3be5d2dc-eab4-4067-b6a9-f32b3b26bc8b_db
-echo "GRANT SELECT ON public.ced_2011_aust TO publicuser;" | psql -U postgres -h postgres -d cartodb_dev_user_3be5d2dc-eab4-4067-b6a9-f32b3b26bc8b_db
-echo "GRANT SELECT ON public.sed_2011_aust TO publicuser;" | psql -U postgres -h postgres -d cartodb_dev_user_3be5d2dc-eab4-4067-b6a9-f32b3b26bc8b_db
-echo "GRANT SELECT ON public.poa_2011_aust TO publicuser;" | psql -U postgres -h postgres -d cartodb_dev_user_3be5d2dc-eab4-4067-b6a9-f32b3b26bc8b_db
+echo "GRANT SELECT ON public.sa1_2011_aust TO publicuser;" | psql -U postgres -h postgres -d $COMMON_DATA_DATABASE
+echo "GRANT SELECT ON public.sa2_2011_aust TO publicuser;" | psql -U postgres -h postgres -d $COMMON_DATA_DATABASE
+echo "GRANT SELECT ON public.sa3_2011_aust TO publicuser;" | psql -U postgres -h postgres -d $COMMON_DATA_DATABASE
+echo "GRANT SELECT ON public.sa4_2011_aust TO publicuser;" | psql -U postgres -h postgres -d $COMMON_DATA_DATABASE
+echo "GRANT SELECT ON public.lga11aaust TO publicuser;" | psql -U postgres -h postgres -d $COMMON_DATA_DATABASE
+echo "GRANT SELECT ON public.ced_2011_aust TO publicuser;" | psql -U postgres -h postgres -d $COMMON_DATA_DATABASE
+echo "GRANT SELECT ON public.sed_2011_aust TO publicuser;" | psql -U postgres -h postgres -d $COMMON_DATA_DATABASE
+echo "GRANT SELECT ON public.poa_2011_aust TO publicuser;" | psql -U postgres -h postgres -d $COMMON_DATA_DATABASE
 
-echo "DELETE FROM public.sa1_2011_aust WHERE ste_code11<>'2';" | psql -U postgres -h postgres -d cartodb_dev_user_3be5d2dc-eab4-4067-b6a9-f32b3b26bc8b_db
-echo "DELETE FROM public.sa2_2011_aust WHERE ste_code11<>'2';" | psql -U postgres -h postgres -d cartodb_dev_user_3be5d2dc-eab4-4067-b6a9-f32b3b26bc8b_db
-echo "DELETE FROM public.sa3_2011_aust WHERE ste_code11<>'2';" | psql -U postgres -h postgres -d cartodb_dev_user_3be5d2dc-eab4-4067-b6a9-f32b3b26bc8b_db
-echo "DELETE FROM public.sa4_2011_aust WHERE ste_code11<>'2';" | psql -U postgres -h postgres -d cartodb_dev_user_3be5d2dc-eab4-4067-b6a9-f32b3b26bc8b_db
-echo "DELETE FROM public.lga11aaust WHERE state_code<>'2';" | psql -U postgres -h postgres -d cartodb_dev_user_3be5d2dc-eab4-4067-b6a9-f32b3b26bc8b_db
-echo "DELETE FROM public.ced_2011_aust WHERE LEFT(ced_code, 1)<>'2';" | psql -U postgres -h postgres -d cartodb_dev_user_3be5d2dc-eab4-4067-b6a9-f32b3b26bc8b_db
-echo "DELETE FROM public.sed_2011_aust WHERE LEFT(sed_code, 1)<>'2';" | psql -U postgres -h postgres -d cartodb_dev_user_3be5d2dc-eab4-4067-b6a9-f32b3b26bc8b_db
-echo "DELETE FROM public.poa_2011_aust WHERE LEFT(poa_code, 1)<>'3';" | psql -U postgres -h postgres -d cartodb_dev_user_3be5d2dc-eab4-4067-b6a9-f32b3b26bc8b_db
+echo "DELETE FROM public.sa1_2011_aust WHERE ste_code11<>'2';" | psql -U postgres -h postgres -d $COMMON_DATA_DATABASE
+echo "DELETE FROM public.sa2_2011_aust WHERE ste_code11<>'2';" | psql -U postgres -h postgres -d $COMMON_DATA_DATABASE
+echo "DELETE FROM public.sa3_2011_aust WHERE ste_code11<>'2';" | psql -U postgres -h postgres -d $COMMON_DATA_DATABASE
+echo "DELETE FROM public.sa4_2011_aust WHERE ste_code11<>'2';" | psql -U postgres -h postgres -d $COMMON_DATA_DATABASE
+echo "DELETE FROM public.lga11aaust WHERE state_code<>'2';" | psql -U postgres -h postgres -d $COMMON_DATA_DATABASE
+echo "DELETE FROM public.ced_2011_aust WHERE LEFT(ced_code, 1)<>'2';" | psql -U postgres -h postgres -d $COMMON_DATA_DATABASE
+echo "DELETE FROM public.sed_2011_aust WHERE LEFT(sed_code, 1)<>'2';" | psql -U postgres -h postgres -d $COMMON_DATA_DATABASE
+echo "DELETE FROM public.poa_2011_aust WHERE LEFT(poa_code, 1)<>'3';" | psql -U postgres -h postgres -d $COMMON_DATA_DATABASE
 
 rake cartodb:remotes:invalidate_common_data
