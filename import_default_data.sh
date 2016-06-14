f716aa2a5fd430d0c2538ac98d60cc2364950948
Jin Park <forjin@vpac-innovations.com.au>
2016-06-14 10:18:40 +1000

    update some of the large file to reduce for importing data library

--- import_default_data.sh
+++ import_default_data.sh
@@ -30,4 +30,6 @@ echo "DELETE FROM public.ced_2011_aust WHERE LEFT(ced_code, 1)<>'2';" | psql -U 
 echo "DELETE FROM public.sed_2011_aust WHERE LEFT(sed_code, 1)<>'2';" | psql -U postgres -h postgres -d $COMMON_DATA_DATABASE
 echo "DELETE FROM public.poa_2011_aust WHERE LEFT(poa_code, 1)<>'3';" | psql -U postgres -h postgres -d $COMMON_DATA_DATABASE
 
+echo "UPDATE external_sources SET size=74399488 WHERE size >= 174399488;" | psql -U postgres -h postgres -d carto_db_development
+
 rake cartodb:remotes:invalidate_common_data
